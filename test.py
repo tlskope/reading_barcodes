4f264edefa9ae8a07180c566f83649c67a859fdf
tlskope <128537840+tlskope@users.noreply.github.com>
2023-06-27 14:05:35 +1200

    working

--- test.py
+++ test.py
@@ -1,16 +1,21 @@
 #%%
 
 import cv2
-from pyzbar.pyzbar import decode 
+from pyzbar.pyzbar import decode
+import csv
 
-img = cv2.imread('test_barcode.png')
-
-print(img)
-
-print (len(decode(img)))
-
+list = []
+img = cv2.imread('test_barcode_3.png')
 for code in decode(img):
     print(code.type)
     print(code.data)
+    print(code.data.decode('utf-8'))
+    list.append(code.data.decode('utf-8'))
 
-# %%
+print(list)
+fields = []
+with open('list_of_barcodes.csv','w') as f:
+    write = csv.writer(f)
+    write.writerows([list])
+
+
